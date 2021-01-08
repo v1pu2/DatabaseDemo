7303d3c066752a1e3dbf559f0fec8b9fa66a9aaf
v1pu2 <v1bhalodia@gmail.com>
2021-01-08 23:17:15 +0530

    set ui in viewlist

--- src/container/ViewList.js
+++ src/container/ViewList.js
@@ -1,15 +1,15 @@
-import React, {useState, useEffect} from 'react';
-import {FlatList, Text, View, SafeAreaView} from 'react-native';
-import {openDatabase} from 'react-native-sqlite-storage';
+import React, { useState, useEffect } from "react";
+import { FlatList, Text, View, StyleSheet, SafeAreaView } from "react-native";
+import { openDatabase } from "react-native-sqlite-storage";
 
-var db = openDatabase({name: 'UserDatabase.db'});
+var db = openDatabase({ name: "UserDatabase.db" });
 
 const ViewAllUser = () => {
   let [flatListItems, setFlatListItems] = useState([]);
 
   useEffect(() => {
     db.transaction((tx) => {
-      tx.executeSql('SELECT * FROM table_user', [], (tx, results) => {
+      tx.executeSql("SELECT * FROM table_user", [], (tx, results) => {
         var temp = [];
         for (let i = 0; i < results.rows.length; ++i)
           temp.push(results.rows.item(i));
@@ -18,46 +18,66 @@ const ViewAllUser = () => {
     });
   }, []);
 
-  let listViewItemSeparator = () => {
-    return (
-      <View
-        style={{
-          height: 0.2,
-          width: '100%',
-          backgroundColor: '#808080',
-        }}
-      />
-    );
-  };
 
   let listItemView = (item) => {
     return (
-      <View key={item.user_id} style={{backgroundColor: 'white', padding: 20}}>
-        <Text>Id: {item.user_id}</Text>
-        <Text>Name: {item.user_name}</Text>
-        <Text>Email: {item.user_email}</Text>
-        <Text>Contact: {item.user_contact}</Text>
-        <Text>Teacher Name: {item && item.teacher}</Text>
-        <Text>Subject: {item && item.subject}</Text>
+      <View key={item.user_id} style={styles.itemConatiner}>
+        <View style={styles.rowView}>
+          <View style={styles.rowView}>
+          <Text style={styles.txtTitle}>Name:</Text>
+          <Text style={styles.text}>{item.user_name}</Text>
+          </View>
+        </View>
+        <View style={styles.rowView}>
+        <View style={styles.rowView}>
+          <Text style={styles.txtTitle}>Email:</Text>
+          <Text style={styles.text}>{item.user_email}</Text>
+          </View>
+          <View style={styles.rowView}>
+          <Text style={styles.txtTitle}>Contact:</Text>
+          <Text style={styles.text}>{item.user_contact}</Text>
+          </View>
+        </View>
+        <View style={styles.rowView}>
+        <View style={styles.rowView}>
+          <Text style={styles.txtTitle}>Teacher Name:</Text>
+          <Text style={styles.text}>{item && item.teacher}</Text>
+          </View>
+          <View style={styles.rowView}>
+          <Text style={styles.txtTitle}>Subject:</Text>
+          <Text style={styles.text}>{item && item.subject}</Text>
+          </View>
+        </View>
       </View>
     );
-
   };
 
   return (
-    <SafeAreaView style={{flex: 1}}>
-      <View style={{flex: 1, backgroundColor: 'white'}}>
-        <View style={{flex: 1}}>
-          <FlatList
-            data={flatListItems}
-            ItemSeparatorComponent={listViewItemSeparator}
-            keyExtractor={(item, index) => index.toString()}
-            renderItem={({item}) => listItemView(item)}
-          />
-        </View>
+    <SafeAreaView style={styles.container}>
+      <View>
+        <FlatList
+          data={flatListItems}
+          keyExtractor={(item, index) => index.toString()}
+          renderItem={({ item }) => listItemView(item)}
+        />
       </View>
     </SafeAreaView>
   );
 };
 
 export default ViewAllUser;
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 10,
+  },
+  itemConatiner: {
+    backgroundColor: "white",
+    padding: 10,
+    margin: 10,
+    borderRadius: 10,
+  },
+  rowView: { flexDirection: "row", marginBottom: 5,paddingRight:10 },
+  txtTitle:{ paddingRight: 10, fontSize: 14,fontWeight:'bold' },
+  text: {  fontSize: 14 },
+});
